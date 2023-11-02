0d20878ab249c6f6b86699b44f360eab834867d0
jeronimo <jeroalbusto00@gmail.com>
2023-11-02 14:27:09 -0300

    se agrego parte de works

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Skills from "./components/Skills/Skills";
+import Work from "./components/Works/Work";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
 import ScrollUp from "./components/Scrollup/ScrollUp";
@@ -18,6 +19,7 @@ function App() {
       <Home/>
       <About/>
       <Skills/>
+      <Work/>
       <Contact/>
     </main>
 
---
+++ src/components/Works/Data.jsx
@@ -0,0 +1,44 @@
+import Work1 from "../../../public/mac.jpg"
+
+export const projectsData = [
+    {
+        id:1,
+        image: Work1,
+        title: 'web design',
+        category: 'web'
+    },
+    {
+        id:2,
+        image: Work1,
+        title: 'App design',
+        category: 'app'
+    },
+    {
+        id:3,
+        image: Work1,
+        title: 'design',
+        category: 'design'
+    },
+    {
+        id:4,
+        image: Work1,
+        title: 'App curse',
+        category: 'app'
+    }
+]
+
+
+export const projectsNav = [
+    {
+        name: 'all',
+    },
+    {
+        name: 'web',
+    },
+    {
+        name: 'app',
+    },
+    {
+        name: 'design',
+    },
+]
